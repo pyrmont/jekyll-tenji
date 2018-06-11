468b91fd713f133da7090198c1c76c060f939573
Michael Camilleri <mike@inqk.net>
2018-06-12 02:43:13 +0900

    Add test for directory with metadata file.

--- lib/tenji/gallery/metadata.rb
+++ lib/tenji/gallery/metadata.rb
@@ -3,19 +3,19 @@ require 'yaml'
 module Tenji
   class Gallery
     class Metadata
-      attr_accessor :name, 
-                    :description, 
-                    :period, 
-                    :singles, 
+      attr_accessor :name,
+                    :description,
+                    :period,
+                    :singles,
                     :paginate
 
       def initialize(file)
         metadata = file.exist? ? YAML.load_file(file.realpath) : {}
-        @name = metadata[:name] || file.parent.basename.to_s
-        @description = metadata[:description] || ''
-        @period = metadata[:period] || ''
-        @singles = metadata[:singles] || false
-        @paginate = metadata[:paginate] || 25
+        @name = metadata["name"] || file.parent.basename.to_s
+        @description = metadata["description"] || ''
+        @period = metadata["period"] || ''
+        @singles = metadata["singles"] || false
+        @paginate = metadata["paginate"] || 25
       end
     end
   end
--- test/test_tenji_gallery_metadata.rb
+++ test/test_tenji_gallery_metadata.rb
@@ -5,8 +5,8 @@ require 'tenji/gallery/metadata'
 class TenjiGalleryMetadataTest < Minitest::Test
   context "Tenji::Gallery::Metadata" do
     context "has an initializer that" do
-      should "return a Tenji::Gallery::Metadata object" do
-        file = Pathname.new 'data/_albums/gallery1/_gallery.yml'
+      should "return a default object when no YAML file exists" do
+        file = Pathname.new 'test/data/_albums/gallery1/_gallery.yml'
         obj = Tenji::Gallery::Metadata.new file
         assert_equal 'Tenji::Gallery::Metadata', obj.class.name
         assert_equal 'gallery1', obj.name
@@ -14,6 +14,17 @@ class TenjiGalleryMetadataTest < Minitest::Test
         assert_equal '', obj.period
         assert_equal false, obj.singles
         assert_equal 25, obj.paginate
+      end
+
+      should "return a custom object when a YAML file exists" do
+        file = Pathname.new 'test/data/_albums/gallery2/_gallery.yml'
+        obj = Tenji::Gallery::Metadata.new file
+        assert_equal 'Tenji::Gallery::Metadata', obj.class.name
+        assert_equal 'Test Gallery', obj.name
+        assert_equal 'An example of a gallery.', obj.description
+        assert_equal '1 January 2018 - 5 January 2018', obj.period
+        assert_equal true, obj.singles
+        assert_equal 15, obj.paginate
       end
     end
   end
