4aa5b4f008a2179775d69bf08c0943608bf13faa
Michael Camilleri <mike@inqk.net>
2018-06-22 15:58:37 +0900

    Update unit tests to properly cover Gallery object.

--- test/test_tenji_gallery.rb
+++ test/test_tenji_gallery.rb
@@ -11,17 +11,26 @@ class TenjiGalleryTest < Minitest::Test
     end
 
     context "has a method #initialize that" do
-      should "return an object if the directory exists" do
+      should "initialize the Gallery object if the directory exists" do
         dir = Pathname.new 'test/data/gallery1/'
-        obj = Tenji::Gallery.new dir, AnyType.new
+        list = Tenji::List.new dir
+        obj = Tenji::Gallery.new dir, list
         assert_equal 'Tenji::Gallery', obj.class.name
-        assert_equal 'Hash', obj.metadata.class.name
+        assert_equal 'gallery1', obj.dirname
         assert_equal [ 'Tenji::Image' ], obj.images.map { |i| i.class.name }
+        assert_equal 'Tenji::List', obj.list.class.name
+        assert_equal 'Hash', obj.metadata.class.name
+        assert_equal '', obj.text
       end
 
       should "raise an error if the file doesn't exist" do
         dir = Pathname.new 'not/a/real/directory'
-        assert_raises(StandardError) { Tenji::Gallery.new dir }
+        assert_raises(StandardError) { Tenji::Gallery.new dir, AnyType.new }
+      end
+      
+      should "raise an error if the list does not exist" do
+        dir = Pathname.new 'test/data/gallery1/'
+        assert_raises(StandardError) { Tenji::Gallery.new dir, nil }
       end
     end
   end
