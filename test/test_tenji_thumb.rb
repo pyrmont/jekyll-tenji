b86010b7aa7e60c09bfc22162c26a3f5384e3916
Michael Camilleri <mike@inqk.net>
2018-06-28 19:09:45 +0900

    Improve test coverage for Thumb object.

--- test/test_tenji_thumb.rb
+++ test/test_tenji_thumb.rb
@@ -3,15 +3,31 @@ require 'test_helper'
 class TenjiThumbTest < Minitest::Test
   context "Tenji::Thumb" do
     setup do
+      Tenji::Config.configure
+      any = AnyType.new
       dir = Pathname.new 'test/data/gallery2'
-      @file = dir + '01-castle.jpg'
-      @obj = Tenji::Thumb.new Hash.new, AnyType.new
+      file = dir + '01-castle.jpg'
+      sizes = Hash.new
+      @image = Tenji::Image.new file, sizes, any
+      @obj = Tenji::Thumb.new sizes, @image
+    end
+
+    teardown do
+      Tenji::Config.reset
     end
 
     context "has a method #initialize that" do
-      should "return an object if passed a path" do
+      should "initialise a Thumb object" do
         assert_equal 'Tenji::Thumb', @obj.class.name
         assert_equal Hash.new, @obj.files
+        assert_equal @image, @obj.image
+      end
+      
+      should "raise an error if the arguments are invalid" do
+        any = AnyType.new
+        t = Tenji::Thumb
+        assert_raises(StandardError) { t.new nil, any }
+        assert_raises(StandardError) { t.new any, nil }
       end
     end
   end
