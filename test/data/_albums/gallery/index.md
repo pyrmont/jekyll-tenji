b4da7d79bc3227f90f98703a54b7634169ef8a4a
Michael Camilleri <mike@inqk.net>
2018-06-28 18:41:17 +0900

    Improve test coverage for List object.

---
+++ test/data/_albums/gallery/index.md
@@ -0,0 +1,7 @@
+---
+title: 'Test Gallery'
+description: 'An example of a gallery.'
+period: '1 January 2018 - 5 January 2018'
+singles: true
+paginate: 15
+---
---
+++ test/test_tenji_list.rb
@@ -0,0 +1,30 @@
+require 'test_helper'
+
+class TenjiListTest < Minitest::Test
+  context "Tenji::List" do
+    setup do
+      Tenji::Config.configure
+    end
+
+    teardown do
+      Tenji::Config.reset
+    end
+
+    context "has a method #initialize that" do
+      should "initialize a List object" do
+        dir = Pathname.new 'test/data/_albums/'
+        obj = Tenji::List.new dir
+        assert_equal 'Tenji::List', obj.class.name
+        assert_equal '_albums', obj.dirname
+        assert_equal [ 'Tenji::Gallery' ], obj.galleries.map { |g| g.class.name }
+        assert_equal 'Hash', obj.metadata.class.name
+        assert_equal '', obj.text
+      end
+
+      should "raise an error if the file doesn't exist" do
+        dir = Pathname.new 'not/a/real/directory'
+        assert_raises(StandardError) { Tenji::List.new dir }
+      end  
+    end
+  end
+end
