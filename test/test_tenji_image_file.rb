80a10247bc65690a93e8fe248df6fc3bde3d9c97
Michael Camilleri <mike@inqk.net>
2018-08-27 15:24:07 +0900

    Improve ImageFile#new tests.

--- test/test_tenji_image_file.rb
+++ test/test_tenji_image_file.rb
@@ -13,13 +13,24 @@ describe Tenji::ImageFile do
   end
     
   describe "#initialize" do
-    it "instantiates a Tenji::ImageFile object" do
+    it "instantiates a Tenji::ImageFile object for a file that exists" do
       obj = Tenji::ImageFile.new @site, @base, '_albums/gallery', '01-castle.jpg'
       assert_equal Tenji::ImageFile, obj.class
       assert_equal 'gallery', obj.gallery_name
       assert_equal 'albums/gallery', obj.instance_variable_get(:@dir)
       assert_equal '01-castle.jpg', obj.name
       assert_equal File.join(@base, '_albums/gallery', '01-castle.jpg'), obj.path
+      assert_nil obj.position
+    end
+    
+    it "instantiates a Tenji::ImageFile object for a file that doesn't exist" do
+      obj = Tenji::ImageFile.new @site, @base, '_albums/gallery', 'foo.jpg'
+      assert_equal Tenji::ImageFile, obj.class
+      assert_equal 'gallery', obj.gallery_name
+      assert_equal 'albums/gallery', obj.instance_variable_get(:@dir)
+      assert_equal 'foo.jpg', obj.name
+      assert_equal File.join(@base, '_albums/gallery', 'foo.jpg'), obj.path
+      assert_nil obj.position
     end
   end
 end
