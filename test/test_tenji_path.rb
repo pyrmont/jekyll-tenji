61aed33e721f54c9d9c489aad50cdba5aaa8c05a
Michael Camilleri <mike@inqk.net>
2018-08-29 09:42:14 +0900

    Add Path#+ tests.

--- test/test_tenji_path.rb
+++ test/test_tenji_path.rb
@@ -21,4 +21,24 @@ describe Tenji::Path do
       assert_equal path, obj.to_s
     end
   end
+
+  describe "#+" do
+    it "returns a Tenji::Path object with a concatenated path if the operand is a Tenji::Path object" do
+      other = Tenji::Path.new 'foo'
+      res = @obj + other
+      assert_equal res.to_s, File.join(@obj.to_s, other.to_s)
+    end
+
+    it "returns a Tenji::Path object with a concatenated path if the operand is a String object" do
+      other = 'foo'
+      res = @obj + other
+      assert_equal res.to_s, File.join(@obj.to_s, other.to_s)
+    end
+
+    it "returns a Tenji::Path object with the same path if the operand is an empty string" do
+      other = ''
+      res = @obj + ''
+      assert_equal res.to_s, @obj.to_s
+    end
+  end
 end
