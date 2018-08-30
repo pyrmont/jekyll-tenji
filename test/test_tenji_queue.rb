54eee452cfbe5809f8eafa1ecf37be6e0de5e057
Michael Camilleri <mike@inqk.net>
2018-08-30 13:36:49 +0900

    Add Queue#initialize tests.

---
+++ test/test_tenji_queue.rb
@@ -0,0 +1,36 @@
+require 'test_helper'
+
+describe Tenji::Queue do
+  describe "#initialize" do
+    it "instantiates a Tenji::Queue object using the default values" do
+      obj = Tenji::Queue.new
+      assert_equal Tenji::Queue, obj.class
+      assert_nil obj.list_page
+      assert_equal Hash.new, obj.gallery_pages
+      assert_equal Hash.new, obj.image_files
+      assert_equal Hash.new, obj.image_pages
+      assert_equal Hash.new, obj.thumb_files
+      assert_equal Hash.new, obj.cover_files
+    end
+
+    it "instantiates a Tenji::Queue object with prepared values" do
+      args = [ 'foo', { 'foo' => 'bar' }, { 'foo' => [ 'bar' ] }, { 'foo' => { 'foo' => 'bar' } }, { 'foo' => { 'foo' => { 'foo' => 'bar' } } }, { 'foo' => 'bar' } ]
+      obj = Tenji::Queue.new args
+
+      assert_equal Tenji::Queue, obj.class
+
+      assert_equal 'foo', obj.list_page
+      assert_equal 'bar', obj.gallery_pages['foo']
+      assert_equal 'bar', obj.image_files['foo'].first
+      assert_equal 'bar', obj.image_pages['foo']['foo']
+      assert_equal 'bar', obj.thumb_files['foo']['foo']['foo']
+      assert_equal 'bar', obj.cover_files['foo']
+
+      assert_nil obj.gallery_pages['bar']
+      assert_equal Array.new, obj.image_files['bar']
+      assert_equal Hash.new, obj.image_pages['bar']
+      assert_equal Hash.new, obj.thumb_files['bar']['bar']
+      assert_nil obj.cover_files['bar']
+    end
+  end
+end
