1510b15b950358ff495ba217accfa45d5ad68aa1
Michael Camilleri <mike@inqk.net>
2018-08-30 15:31:55 +0900

    Remove unnecessary modules.

--- lib/jekyll-tenji.rb
+++ lib/jekyll-tenji.rb
@@ -13,14 +13,12 @@ require 'tenji/refinements'
 require 'tenji/config'
 require 'tenji/path'
 require 'tenji/queue'
-require 'tenji/utilities'
 require 'tenji/writer'
 
 require 'tenji/generator'
 
 require 'tenji/processable'
 require 'tenji/pageable'
-require 'tenji/scalable'
 
 require "tenji/gallery_page"
 require "tenji/image_file"
